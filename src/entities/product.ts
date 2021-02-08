c297a66e257951822cfc332daa27113b3e013680
jsotoca <jsotoca@unprg.edu.pe>
2021-02-07 21:13:27 -0500

    Add product entity

---
+++ src/entities/product.ts
@@ -0,0 +1,17 @@
+export default class Product {
+    
+    id: number;
+    name: string;
+    price: number;
+    created_at: Date;
+    updated_at: Date;
+
+    constructor(id: number, name: string, price: number, created_at: Date, updated_at: Date){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+    
+}
