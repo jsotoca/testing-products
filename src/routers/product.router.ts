08a1b99cff41dee4dfa645045b365efa9224de50
jsotoca <jsotoca@unprg.edu.pe>
2021-02-07 21:29:00 -0500

    Add getProducts to router get / in productRouter

---
+++ src/routers/product.router.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { getProducts } from '../controllers/product.controller';
+
+const productRouter = Router();
+
+productRouter.get('/', getProducts);
+
+export default productRouter;
