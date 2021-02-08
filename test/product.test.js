07067d5e660c712ee86212951397fb1063bff0fa
jsotoca <jsotoca@unprg.edu.pe>
2021-02-08 01:13:52 -0500

    Add env variables to test

--- test/product.test.js
+++ test/product.test.js
@@ -1,10 +1,11 @@
 
 const expect = require('chai').expect;
 const request = require('request');
+const env = require('./env');
 
 describe('Products Tests', () => {
     it('Get All Products', (done) => {
-        request('http://localhost:4000/api/product', (err, response, body) => {
+        request(`${env.APP_URL}/product`, (err, response, body) => {
             body = JSON.parse(body);
             expect(response.statusCode).to.equal(200);
             expect(body).to.have.property('products');
